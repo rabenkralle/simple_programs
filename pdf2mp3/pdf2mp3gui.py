4360f2e61defe4c27aad33c86a7b63280b194450
Rashid Imanov <rush.imanov@gmail.com>
2022-07-28 14:41:32 +0300

    pdf2mp3_gui

--- pdf2mp3/pdf2mp3gui.py
+++ pdf2mp3/pdf2mp3gui.py
@@ -1,3 +1,6 @@
+# pdf 2 mp3 converter версия с GUI
+
+
 from gc import is_finalized
 import pdfplumber
 from gtts import gTTS
@@ -50,18 +53,3 @@ root.title("Guess My Number")
 app = Application(root)
 root.mainloop()
 
-# def pdf2mp3(file_path, lang):
-#     if Path(file_path).is_file() and Path(file_path).suffix == '.pdf':
-#         with pdfplumber.PDF(open(file=file_path, mode='rb')) as pdf:
-#             pages = [page.extract_text() for page in pdf.pages]
-
-#         pdf_text = ''.join(pages)
-#         pdf_text = pdf_text.replace('\n', '')
-
-#         audio_file = gTTS(text=pdf_text, lang=lang, slow=False)
-#         audio_name = Path(file_path).stem
-#         audio_file.save(f'{audio_name}.mp3')
-
-#         return f'{audio_name}.mp3 saved!'
-#     else:
-#         return 'File not exists'
