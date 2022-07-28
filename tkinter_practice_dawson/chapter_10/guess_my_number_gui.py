7e1ae0798f3eb2bac8c0f9130dd6dcd68cefca94
Rashid Imanov <rush.imanov@gmail.com>
2022-07-28 12:09:26 +0300

    chapter_10

--- tkinter_practice_dawson/chapter_10/guess_my_number_gui.py
+++ tkinter_practice_dawson/chapter_10/guess_my_number_gui.py
@@ -1,30 +1,16 @@
+# Guess My Number
+#
+# The computer picks a random number between 1 and 100
+# The player tries to guess it and the computer lets
+# the player know if the guess is too high, too low
+# or right on the money
+
 import random  
 from tkinter import *
 
-# print("\tWelcome to 'Guess My Number'!")
-# print("\nI'm thinking of a number between 1 and 100.")
-# print("Try to guess it in as few attempts as possible.\n")
-
-# # set the initial values
-# the_number = random.randint(1, 100)
-# guess = int(input("Take a guess: "))
-# tries = 1
-
-# # guessing loop
-# while guess != the_number:
-#     if guess > the_number:
-#         print("Lower...")
-#     else:
-#         print("Higher...")
-            
-#     guess = int(input("Take a guess: "))
-#     tries += 1
-
-# print("You guessed it!  The number was", the_number)
-# print("And it only took you", tries, "tries!\n")
 
 class Application(Frame):
-    # GUI application that creates a story based on user input. 
+    # GUI application where player tries to guess random number. 
     def __init__(self, master):
         super(Application, self).__init__(master)
         self.grid()
