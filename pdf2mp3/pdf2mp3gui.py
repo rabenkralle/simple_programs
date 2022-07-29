a73998caee055dd2e142f3425fabebd0ef5bc3c5
Rashid Imanov <rush.imanov@gmail.com>
2022-07-29 12:35:36 +0300

    pdf2mp3_gui

--- pdf2mp3/pdf2mp3gui.py
+++ pdf2mp3/pdf2mp3gui.py
@@ -2,6 +2,7 @@
 
 
 from gc import is_finalized
+from matplotlib.pyplot import text
 import pdfplumber
 from gtts import gTTS
 from pathlib import Path
@@ -19,6 +20,15 @@ class Application(Frame):
     def create_widgets(self):
         Label(self, text="Choose pdf file").grid(row=0, 
         column=0, columnspan=2, sticky=W)
+        Label(self, text="Choose language:").grid(row=1, column=0, sticky=W)
+        self.body_part = StringVar()
+        self.body_part.set(None)
+        body_parts = ["English", "Russian"]
+        column = 1
+        for part in body_parts:
+            Radiobutton(self, text=part, variable=self.body_part, 
+            value=part).grid(row=1, column=column, sticky=W)
+            column += 1
         ttk.Button(self, text="Open file", command=self.select_file).grid(row=2, 
         column=0, sticky=W)
         
@@ -32,20 +42,35 @@ class Application(Frame):
             title='Open a file',
             initialdir='/',
             filetypes=filetypes)
-        print(filename)
+        
         showinfo(
             title='Selected File',
             message=filename
         )
-        with pdfplumber.PDF(open(file=filename, mode='rb')) as pdf:
-            pages = [page.extract_text() for page in pdf.pages]
-        
-        pdf_text = ''.join(pages)
-        pdf_text = pdf_text.replace('\n', '')
 
-        audio_file = gTTS(text=pdf_text, lang='ru', slow=False)
-        audio_name = Path(filename).stem
-        audio_file.save(f'{audio_name}.mp3')
+        if self.body_part.get() == 'English':
+            lang = 'en'
+        else:
+            lang = 'ru'
+
+        if Path(filename).is_file() and Path(filename).suffix == '.pdf':
+            with pdfplumber.PDF(open(file=filename, mode='rb')) as pdf:
+                pages = [page.extract_text() for page in pdf.pages]
+            
+            pdf_text = ''.join(pages)
+            pdf_text = pdf_text.replace('\n', '')
+
+            audio_file = gTTS(text=pdf_text, lang=lang, slow=False)
+            audio_name = Path(filename).stem
+            audio_file.save(f'{audio_name}.mp3')
+            showinfo(
+                title='Saved',
+                message=f'{audio_name}.mp3 saved'
+            )
+        else:
+            showinfo(
+                title='Error',
+                message='Choose PDF file')
 
 
 root = Tk()
